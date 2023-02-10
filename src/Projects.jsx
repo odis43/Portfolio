256ed90ca953733dc9f92e41e8b864bf85a5c2ae
Oditha <oditha14@gmail.com>
2023-02-10 13:04:37 -0500

    changes to project modals

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -40,20 +40,23 @@ const Projects = forwardRef((props, ref) => {
               <div className="modal-content">
                 {" "}
                 <p className="modal-para">
-                  Created a full-stack web app with React and React-Bootstrap
-                  and utilized the BLIP API (Bootstrapping Language-Imaging
-                  Pre-Training) for image captioning. This was a duo effort
-                  between my friend and I and we were very new to full-stack dev
-                  especially using React. By the end of the project, we
-                  developed a good understanding of class components.
+                  A full-stack web app with React, Bootstrap and the BLIP API
+                  (Bootstrapping Language-Imaging Pre-Training) for image
+                  captioning. Challenge Drawdle with your drawing skills to see
+                  who comes on top! Unfortunately, the BLIP API is now outdated
+                  meaning Drawdle's guesses are slower. Although, feel free to
+                  make some fun doodles within the app.
                 </p>
                 <a href="https://github.com/odis43/Drawdle" target="_blank">
                   <GitIcon />
                 </a>
-                <a href="drawdle.vercel.app" target="_blank">
+                <a href="https://drawdle.vercel.app" target="_blank">
                   <ChainIcon className="chain" />
                 </a>
-                <div></div>
+                <div style={{ "padding-top": "5px" }}>
+                  <h3>Skills</h3>
+                  <p>React js | Node js | Rest API</p>
+                </div>
               </div>
             </div>
           )}
@@ -78,12 +81,19 @@ const Projects = forwardRef((props, ref) => {
               <div className="modal-content">
                 {" "}
                 <p className="modal-para">
-                  No idea what the fuck this is LMAO
+                  Have you ever wanted to have "more" from a song? You end up
+                  searching endlessly to find another tune that sounds just like
+                  your favorite but find dissapointing results. Similarfy
+                  puts an end to this problem by intelligently searching for
+                  similar songs to a given search using the Spotify API.
                 </p>
                 <a href="https://github.com/odis43/Nostalgify" target="_blank">
                   <GitIcon />
                 </a>
-                <div></div>
+                <div style={{ "padding-top": "5px" }}>
+                  <h3>Skills</h3>
+                  <p>Next js | Node js | Spotify API</p>
+                </div>
               </div>
             </div>
           )}
@@ -108,17 +118,18 @@ const Projects = forwardRef((props, ref) => {
               <div className="modal-content">
                 {" "}
                 <p className="modal-para">
-                  Used pre-parsed Twitter data to train an NLP model to classify sentences as "positive" or 
-                  "negative".  Data was split into training and testing sets.  The model was then evaluated and yielded an accuracy
-                  of 85%.
+                  Used pre-parsed Twitter data to train an NLP model to classify
+                  sentences as "positive" or "negative". Data was split into
+                  training and testing sets. The model was then evaluated and
+                  yielded an accuracy of 85%.
                 </p>
-                <a href="https://github.com/odis43/Drawdle" target="_blank">
+                <a href="" target="_blank">
                   <GitIcon />
                 </a>
-                <a href="drawdle.vercel.app" target="_blank">
-                  <ChainIcon className="chain" />
-                </a>
-                <div></div>
+                <div style={{ "padding-top": "5px" }}>
+                  <h3>Skills</h3>
+                  <p>Python | NLP | Sentiment Analysis</p>
+                </div>
               </div>
             </div>
           )}
@@ -143,13 +154,21 @@ const Projects = forwardRef((props, ref) => {
               <div className="modal-content">
                 {" "}
                 <p className="modal-para">
-                  Since I love music I thought it would be fitting to create a portfolio that resembled an artist's 
-                  Spotify page.  The portfolio was built using React Js components.
+                  I love music. Ever since I was a child, music has been an
+                  innate part of my life. I thought it was fitting to create a
+                  personal portfolio that takes the form of an artist's Spotify
+                  bio - mixing my love for development and music.
                 </p>
-                <a href="" target="_blank">
+                <a href="https://github.com/odis43/Portfolio" target="_blank">
                   <GitIcon />
                 </a>
-                <div></div>
+                <a href="https://github.com/odis43/Portfolio" target="_blank">
+                  <ChainIcon className="chain" />
+                </a>
+                <div style={{ "padding-top": "5px" }}>
+                  <h3>Skills</h3>
+                  <p>React js | Node js | Rest API</p>
+                </div>
               </div>
             </div>
           )}
