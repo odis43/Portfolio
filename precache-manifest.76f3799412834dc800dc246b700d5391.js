7d4aa2f029e573fe13643cecce3df8b523e33cd3
Oditha <oditha14@gmail.com>
2023-02-09 23:51:53 -0500

    Updates

---
+++ precache-manifest.76f3799412834dc800dc246b700d5391.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5e496ca2996eab03e8f",
+    "url": "/Portfolio/static/css/main.d202e960.chunk.css"
+  },
+  {
+    "revision": "b5e496ca2996eab03e8f",
+    "url": "/Portfolio/static/js/main.b5e496ca.chunk.js"
+  },
+  {
+    "revision": "bc55bf15ef120fe301fe",
+    "url": "/Portfolio/static/js/1.bc55bf15.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "30963326c69d549ba692e660a771bbf7",
+    "url": "/Portfolio/static/media/playround.30963326.svg"
+  },
+  {
+    "revision": "0722b0c3cb6ee3cb8481001593479287",
+    "url": "/Portfolio/static/media/Oditha A resume.0722b0c3.pdf"
+  },
+  {
+    "revision": "f9404f2a7749ae915117ac325c5cfd49",
+    "url": "/Portfolio/static/media/dog.f9404f2a.jpg"
+  },
+  {
+    "revision": "98fc406abd85a22ae5c9ee79c0ef7460",
+    "url": "/Portfolio/static/media/drawdle.98fc406a.png"
+  },
+  {
+    "revision": "cde82500e8b944b228a8bb6489f95427",
+    "url": "/Portfolio/static/media/nostalgic.cde82500.jpg"
+  },
+  {
+    "revision": "f2d0a90389881da81870573ab7004827",
+    "url": "/Portfolio/static/media/sentiment.f2d0a903.png"
+  },
+  {
+    "revision": "445b9cf901fa391a1332e04bd999f2c9",
+    "url": "/Portfolio/static/media/website.445b9cf9.jpeg"
+  },
+  {
+    "revision": "0e875127f2d80e2ef2419c38c332aba7",
+    "url": "/Portfolio/static/media/bg.0e875127.jpg"
+  },
+  {
+    "revision": "9229f0698e94baf8cb47fd8a8a79c90b",
+    "url": "/Portfolio/static/media/play.9229f069.svg"
+  },
+  {
+    "revision": "b9678c54c40c00486901068f09357907",
+    "url": "/Portfolio/static/media/artist.b9678c54.svg"
+  },
+  {
+    "revision": "3f1151535daf8b89bf9196867f02de35",
+    "url": "/Portfolio/static/media/follow.3f115153.svg"
+  },
+  {
+    "revision": "0afa412282e01bf8e7feef0e0ec4258a",
+    "url": "/Portfolio/static/media/linkedin.0afa4122.svg"
+  },
+  {
+    "revision": "4ca48353b0ad0407d4f27ec95e49cebc",
+    "url": "/Portfolio/static/media/git.4ca48353.svg"
+  },
+  {
+    "revision": "1cb038866c9d7d21f5d9a5fda423889f",
+    "url": "/Portfolio/static/media/spotify.1cb03886.svg"
+  },
+  {
+    "revision": "3efd23442240d5200b586790b251c6ab",
+    "url": "/Portfolio/static/media/home.3efd2344.svg"
+  },
+  {
+    "revision": "aa192a4c944ce8595ca7d5bdfe787aaa",
+    "url": "/Portfolio/static/media/search.aa192a4c.svg"
+  },
+  {
+    "revision": "5fc1e5af11664ecafc5a8d8013ca3dc4",
+    "url": "/Portfolio/static/media/library.5fc1e5af.svg"
+  },
+  {
+    "revision": "911cfbab2e393c6d1aec7298ac3e58b3",
+    "url": "/Portfolio/static/media/radio.911cfbab.svg"
+  },
+  {
+    "revision": "2818295e62958269f4bf6adad4869f60",
+    "url": "/Portfolio/static/media/resume.2818295e.svg"
+  },
+  {
+    "revision": "8f2631d4706eefc948b90f08012e7de9",
+    "url": "/Portfolio/static/media/manulife.8f2631d4.svg"
+  },
+  {
+    "revision": "47096806bfa69841b288a49c6c6de71a",
+    "url": "/Portfolio/static/media/chain.47096806.svg"
+  },
+  {
+    "revision": "ecaa9d43504223dff195bc3226a9a145",
+    "url": "/Portfolio/index.html"
+  }
+];
