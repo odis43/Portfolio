bbccbdb4ccfbb41453e2cedab71125747557bd27
Oditha <oditha14@gmail.com>
2023-10-31 02:19:57 -0400

    pls deploy

---
+++ updateall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update Node.js and npm
+npm install -g npm@latest
+
+# Update or install individual packages
+npm install fsevents@2
+npm install uuid@latest
+npm install urix@latest
+npm install source-map-url@latest
+npm install flatten@latest
+npm install kleur@latest
+npm install left-pad@latest
+npm install @babel/plugin-transform-class-properties@latest
+npm install flatted@latest
+npm install sane@latest
+npm install topo@latest
+npm install hoek@latest
+npm install @babel/eslint-parser
+npm install @babel/plugin-transform-optional-catch-binding
+npm install acorn-dynamic-import@latest
+npm install @babel/plugin-transform-object-rest-spread
+npm install @babel/plugin-transform-json-strings
+npm install joi@latest
+npm install core-js@latest
+npm install @babel/plugin-transform-unicode-property-regex
+npm install svgo@latest
+
+# Print a message indicating the update is complete
+echo "All package updates/installations are complete."
+
